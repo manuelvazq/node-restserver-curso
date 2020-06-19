64cc1e1e1bf9e7f5ccdc55dea1692ae9ca5c8c0b
Manuel <mvazquez@actualg.com>
2020-06-19 12:57:16 -0500

    login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,17 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //========================================
+// Vencimiento del Token
+//========================================
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//========================================
+// SEED de autenticacion
+//========================================
+
+process.env.SEED = process.env.SEED || 'Este-es-el-seed-desarrollo';
+
+//========================================
 // Base de datos
 //========================================
 
